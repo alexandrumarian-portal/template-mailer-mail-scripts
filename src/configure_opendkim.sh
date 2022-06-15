7789ee0dfe9c7b981c35443ac603d10ef67ab766
milosob <git@milosob.com>
2022-06-15 21:00:20 +0200

    refactoring

--- src/configure_opendkim.sh
+++ src/configure_opendkim.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 
-mkdir -p "$DKIM_DATA"
-setfacl -PRdm u::rwx,g::rwx,o::- "$DKIM_DATA"
+mkdir -p "$DKIM_CONFIG_DATA"
+setfacl -PRdm u::rwx,g::rwx,o::- "$DKIM_CONFIG_DATA"
 
-mkdir -p "$DKIM_DATA_KEYS"
-chgrp -R "$DKIM_GROUP" "$DKIM_DATA_KEYS"
-chmod g+s "$DKIM_DATA_KEYS"
+mkdir -p "$DKIM_CONFIG_DATA_KEYS"
+chgrp -R "$DKIM_GROUP" "$DKIM_CONFIG_DATA_KEYS"
+chmod g+s "$DKIM_CONFIG_DATA_KEYS"
 
 FILE="$DKIM_CONFIG"
 backup "$FILE"
@@ -24,10 +24,10 @@ UserID			            $DKIM_USER:$DKIM_GROUP
 PidFile			            $DKIM_USER_HOME/opendkim.pid
 Socket			            inet:14291@localhost
 
-KeyTable                    refile:$DKIM_DATA_KEY_TABLE
-SigningTable                refile:$DKIM_DATA_SIGNING_TABLE
-InternalHosts               refile:$DKIM_DATA_TRUSTED_HOSTS
-ExternalIgnoreList          refile:$DKIM_DATA_EXTERNAL_IGNORE_LIST
+KeyTable                    refile:$DKIM_CONFIG_DATA_KEY_TABLE
+SigningTable                refile:$DKIM_CONFIG_DATA_SIGNING_TABLE
+InternalHosts               refile:$DKIM_CONFIG_DATA_TRUSTED_HOSTS
+ExternalIgnoreList          refile:$DKIM_CONFIG_DATA_EXTERNAL_IGNORE_LIST
 
 Mode        	            sv
 OversignHeaders		        From
@@ -36,30 +36,30 @@ TrustAnchorFile		        /usr/share/dns/root.key
 
 EOF
 
-FILE="$DKIM_DATA_KEY_TABLE"
+FILE="$DKIM_CONFIG_DATA_KEY_TABLE"
 cat <<EOF >"$FILE"
-# mail._domainkey.example.com example.com:mail:$DKIM_DATA_KEYS/example.com/mail.private
+# mail._domainkey.example.com example.com:mail:$DKIM_CONFIG_DATA_KEYS/example.com/mail.private
 
 EOF
 
-FILE="$DKIM_DATA_SIGNING_TABLE"
+FILE="$DKIM_CONFIG_DATA_SIGNING_TABLE"
 cat <<EOF >"$FILE"
 # *@example.com mail._domainkey.example.com
 
 EOF
 
-FILE="$DKIM_DATA_TRUSTED_HOSTS"
+FILE="$DKIM_CONFIG_DATA_TRUSTED_HOSTS"
 cat <<EOF >"$FILE"
 # example.com
 # *.example.com
 
 EOF
 
-FILE="$DKIM_DATA_EXTERNAL_IGNORE_LIST"
+FILE="$DKIM_CONFIG_DATA_EXTERNAL_IGNORE_LIST"
 cat <<EOF >"$FILE"
 # domain.com
 # *.domain.com
 
 EOF
 
-sudo chmod -R 770 "$DKIM_DATA"
+sudo chmod -R 770 "$DKIM_CONFIG_DATA"
